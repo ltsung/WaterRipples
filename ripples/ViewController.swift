9b5b776a6d328987c890600f42e48e5ba18ee943
Lauren Tsung <ltsung@yahoo-inc.com>
2014-10-05 19:48:37 -0700

    Starting the water ripple project

---
+++ ripples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ripples
+//
+//  Created by Lauren Tsung on 10/5/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\ltsung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ripplesTests/ripplesTests.swift
@@ -0,0 +1,36 @@
+//
+//  ripplesTests.swift
+//  ripplesTests
+//
+//  Created by Lauren Tsung on 10/5/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\ltsung. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ripplesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
